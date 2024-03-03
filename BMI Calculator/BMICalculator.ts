24a2192a9a3a725e46db078d888d989f00ca9543
Muhammad Fuzail <159969929+MuhammadFuzail591@users.noreply.github.com>
2024-03-04 00:22:48 +0500

    Add files via upload

---
+++ BMI Calculator/BMICalculator.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer"
+import chalk from "chalk"
+console.log(`----------BMI Calculator----------`)
+let Weight=await inquirer.prompt([{
+    name:"YourWeight",
+    type:"number",
+    message:"Enter Your Weight please:"
+}])
+
+let HeightInMeters=await inquirer.prompt([{
+    name:"YourHeight",
+    type:"number",
+    message:"Enter Your Height in meters:"
+}])
+
+let BMI=Weight.YourWeight/(HeightInMeters.YourHeight*HeightInMeters.YourHeight)
+
+console.log(`Your BMI is:` + BMI)
+
+if((BMI>18.5)&&(BMI<25)){
+    console.log(chalk.blue(`Congrats!! You are healthy weight`))
+}
+else if(BMI<18.5){
+    console.log(chalk.red(`Sorry!! You are UnderWeight`))
+}
+else{
+    console.log(chalk.red(`Sorry!! You are OverWeight`))
+}
